a006c1bdeb3fd69ca8e96285e1248d2866912d41
Franckyi <franck.velasco@hotmail.fr>
2017-01-24 21:40:14 +0100

    1.11.x-0.1.0.0 - Small fixes, first official release

--- src/main/java/com/franckyi/itemeditor/ModReference.java
+++ src/main/java/com/franckyi/itemeditor/ModReference.java
@@ -4,7 +4,7 @@ public class ModReference {
 
 	public static final String MODID = "itemeditor";
 	public static final String NAME = "Item Editor GUI";
-	public static final String VERSION = "1.11.x-0.1.0.0-beta2";
+	public static final String VERSION = "1.11.x-0.1.0.0";
 	public static final String MCVERSION = "[1.11,1.11.2]";
 
 	public static final String CLIENT_PROXY_CLASS = "com.franckyi.itemeditor.proxy.ClientProxy";
